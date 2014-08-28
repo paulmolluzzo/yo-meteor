8b9d983c1d8c08de02df8e204fe5cc650e574f34
Paul Molluzzo <paul@molluzzo.com>
2014-08-27 23:09:56 -0400

    Update for meteor packages; fix type bug  with attaching links

--- package.js
+++ package.js
@@ -1,9 +1,13 @@
 Package.describe({
-  summary: "Yo API wrapper for Meteor"
+  summary: "Yo API wrapper for Meteor",
+  git: "https://github.com/paulmolluzzo/yo-meteor.git",
+  version: "1.0.0",
+  name: "paulm:yo-meteor",
 });
 
 Package.on_use(function (api, where) {
-    api.export && api.export('yoAPI', 'server');
+  api.versionsFrom("METEOR@0.9.0");
+    api.export('yoAPI', 'server');
     api.add_files('lib/yo.js', ['client', 'server']);
 });
 
